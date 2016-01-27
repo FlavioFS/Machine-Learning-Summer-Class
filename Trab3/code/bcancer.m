00c946de5e96addab1144ceb32431c270095cda0
FlavioFS <flaviofreitas.h@gmail.com>
2016-01-27 11:44:37 -0300

    Day 2

--- Trab3/code/bcancer.m
+++ Trab3/code/bcancer.m
@@ -7,8 +7,19 @@ COLUMNS = size(wdbc(1,:))(1,2);  % Number of columns of the first line
 breakpoint = floor(TOTAL*0.7);	 % Separating the trainin from the prediction
 
 %% Creating the Xt matrix
-training_X = wdbc(1:breakpoint, 3:COLUMNS); % 3rd to last columns
-training_X = zscore(training_X);            % Standard Score of Columns ("Normalizing")
+training_X = wdbc(1:breakpoint, 3:COLUMNS);                         % 3rd to last columns
+training_X = zscore(training_X);                                    % Standard Score of Columns ("Normalizing")
+training_X = [ones(breakpoint, 1) training_X(1:breakpoint, 1:end)]; % 1's for constant term
 
 %% Creating the Yt matrix
-training_Y = wdbc(1:breakpoint, 2);         % 2nd column
+training_Y = wdbc(1:breakpoint, 2); % 2nd column
+
+%% Phi: Error (evaluation function)
+% function error = U(w, )
+% 	error = 1 / (1 + exp());
+% end
+
+%% Phi: Error (evaluation function)
+% function error = Phi(u)
+% 	error = 1 / (1 + exp());
+% end
