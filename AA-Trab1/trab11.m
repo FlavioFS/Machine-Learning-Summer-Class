38732bed00add92d25d57753589cf330738cabe4
FlavioFS <flaviofreitas.h@gmail.com>
2016-04-04 13:21:55 -0300

    bug in core formula

--- AA-Trab1/trab11.m
+++ AA-Trab1/trab11.m
@@ -7,23 +7,44 @@ Y = ex1data1(:, 2);
 DATA_AMOUNT = size(X)(1,1);
 ARGS_AMOUNT = size(X)(1,2);
 W = ones(1, ARGS_AMOUNT+1) * 0.1;
+X = [X(:,1) zscore(X(:, 2:end))];
 X = [ones(DATA_AMOUNT, 1) X];
-X = transpose(X);
-Y = transpose(Y);
 % -----------------------------
 
-% -------- Definitions --------
-ALPHA = 0.0001;
 
+% Constants
+ALPHA = 0.001;
+PASSES = 1;
+
+
+% --------- Functions ---------
 %% Yp: Estimates Y(i)
-function outputs = Yp(i, W, X)
-	outputs = W*X(:, i);
+function outputs = Yp(i, W, Xt)
+	outputs = W*Xt(:, i);
 end
 
 %% e: Error
-function outputs = e(i, W, X, Y)
-	outputs = Yp(i, W, X) - Y(i);
+function outputs = e(i, W, Xt, Yt)
+	outputs = Yp(i, W, Xt) - Yt(i);
 end
+% -----------------------------
 
-Yp(1, W, X)
-e(1, W, X, Y)
+
+% --------- Training ----------
+for pass_it = 1 : PASSES
+	% Shuffle
+	newOrder = randperm(DATA_AMOUNT, DATA_AMOUNT);
+
+	Xs = X(newOrder, :);
+	Ys = Y(newOrder, :);
+
+	Xt = transpose(Xs);
+	Yt = transpose(Ys);
+
+	% Practice
+	for sample_it = 1 : DATA_AMOUNT
+		W
+		ALPHA * e(sample_it, W, Xt, Yt) * X(sample_it, :)
+		W = W + ALPHA * e(sample_it, W, Xt, Yt) * X(sample_it, :);
+	end
+end
